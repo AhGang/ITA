1effe406dc28b3628daa542b59156c076e87202f
AhGang <474494594@qq.com>
2019-07-15 21:02:27 +0800

    delete project

--- dml.sql
+++
@@ -1,8 +0,0 @@
--- 插入记录  
-INSERT INTO tableName (列1，列2) values(value1,value2);
--- 修改记录  
-UPDATE tableName  set 列=value where id=”1“;
---  删除记录  
-DELETE FROM tableName  where id=”1“;
--- 查询记录  
-SELECT * FROM tableName;
